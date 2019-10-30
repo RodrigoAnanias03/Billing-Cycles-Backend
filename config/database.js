d3c1c98860e5fb4b3ae467768e4abc7ff4d99d52
RodrigoAnanias03 <r.ananias03@gmail.com>
2019-10-30 08:33:56 -0300

    Translate error messages

--- config/database.js
+++ config/database.js
@@ -6,3 +6,7 @@ module.exports = mongoose.connect('mongodb://localhost/db_finance', {
   })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio"
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite maximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é valido para o atributo '{PATH}'."
+
