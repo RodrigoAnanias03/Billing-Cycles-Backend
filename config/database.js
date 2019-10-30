c6a9bddeff56063f9b80a949fd0787b9a1c2a6f6
RodrigoAnanias03 <r.ananias03@gmail.com>
2019-10-30 08:22:46 -0300

    Config error messages

--- config/database.js
+++ config/database.js
@@ -4,3 +4,5 @@ module.exports = mongoose.connect('mongodb://localhost/db_finance', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio"
